9faa9581d92ae726863b495dca15d5e8bf23ada5
Sennaf0x <Sennafo@hotmail.com>
2025-05-29 00:38:49 -0400

    Exercicio II parte 2

--- WACAD009-EXPRESS/Exercicio II/build/index.js
+++ WACAD009-EXPRESS/Exercicio II/build/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const validateEnv_1 = __importDefault(require("./utils/validateEnv"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+(0, validateEnv_1.default)();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3333;
 app.get("/", (req, res) => {
