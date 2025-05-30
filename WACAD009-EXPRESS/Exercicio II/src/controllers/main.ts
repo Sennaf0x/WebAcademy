ef969108105ff082ef7cc059641ab8b789de6c15
Sennaf0x <Sennafo@hotmail.com>
2025-05-29 23:29:30 -0400

    Exercicio II - parte 11 arrumando rotas e layouts

--- WACAD009-EXPRESS/Exercicio II/src/controllers/main.ts
+++ WACAD009-EXPRESS/Exercicio II/src/controllers/main.ts
@@ -14,7 +14,11 @@ const lorem = (req: Request, res: Response) => {
             
             const numParagraphs = parseInt(req.params.paragraphs as string) || 1;
             const result = loremIpsum(numParagraphs);
-            res.send(result);
+            res.render('main/lorem', {
+                title:"lorem",
+                result: `${result}`,
+                layout: 'main'
+            })
     }
 
 const hb1 = (req: Request, res: Response) => {
@@ -55,23 +59,64 @@ const hb4 = (req: Request, res: Response) => {
                             { name: 'Docker', type: 'Virtualization', poweredByNodejs: false },
                             { name: 'Sequelize', type: 'ORM tool', poweredByNodejs: true },
                         ];
-                        // Usa o helper para gerar a lista de tecnologias
-                        const techListHTML = listTechs(technologies);
-                        
-                        // Monta a resposta HTML
-                        const responseHTML = `
+    const techListHTML = listTechs(technologies);                                            
+    const responseHTML = `
                         <!DOCTYPE html>
                         <html lang="en">
                         <head>
                             <meta charset="UTF-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
                             <title>Tecnologias com Node.js</title>
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+   
                         </head>
                         <body>
-                            <div class='container'>
-                                <h1>Tecnologias Desenvolvidas com Node.js</h1>
-                                ${techListHTML} <!-- Insere a lista de tecnologias aqui -->
+                        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                            <div class="container-fluid">
+                                <a class="navbar-brand" href="#">Express</a>
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="/hb1">Hb1</a>
+                                    </li>
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="/hb2">Hb2</a>
+                                    </li>
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="/hb3">Hb3</a>
+                                    </li>
+                                    <li class="nav-item">
+                                    <a class="nav-link" href="/hb4">Hb4</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Lorem
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="/lorem/1">1</a></li>
+                                        <li><a class="dropdown-item" href="/lorem/2">2</a></li>
+                                        <li><a class="dropdown-item" href="/lorem/3">3</a></li>
+                                    </ul>
+                                    </li>
+                                </ul>
+                                <form class="d-flex" role="search">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                                </div>
                             </div>
+                        </nav>
+
+                        <div class='container'>
+                            <h1>Tecnologias Desenvolvidas com Node.js</h1>
+                            ${techListHTML}
+                        </div>
+                        <footer>
+                        <p>© 2023 Meu Aplicativo. Todos os direitos reservados.</p>
+                        </footer>
                         </body>
                         </html>`;
                         
