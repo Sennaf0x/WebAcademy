27ce38fc28ad5011d80a881af539005747fcd928
Sennaf0x <Sennafo@hotmail.com>
2025-05-29 23:55:33 -0400

    Exercicio II parte 11 ultimos ajustes

--- WACAD009-EXPRESS/Exercicio II/src/views/helpers/helpers.ts
+++ WACAD009-EXPRESS/Exercicio II/src/views/helpers/helpers.ts
@@ -3,6 +3,7 @@ import { Tech } from './helpersTypes';
 export function listTechs(technologies: Tech[]) {
     const list = technologies
         .filter(tech => tech.poweredByNodejs)
-        .map(tech => `<li>${tech.name} - ${tech.type}</li>`); 
-    return `<ul>${list.join('')}</ul>`;
+        .map(tech => `${tech.name} - ${tech.type}`);
+         
+    return list;
 }
