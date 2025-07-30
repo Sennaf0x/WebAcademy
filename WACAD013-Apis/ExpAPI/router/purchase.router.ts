414c69c62e5cce52730ece849f892dd507f398c9
Sennaf0x <Sennafo@hotmail.com>
2025-07-30 03:29:15 -0400

    Exercicio IV Adicionando logica de compra e carrinho

--- WACAD013-Apis/ExpAPI/router/purchase.router.ts
+++ WACAD013-Apis/ExpAPI/router/purchase.router.ts
@@ -4,7 +4,7 @@ import * as purchaseController from '../controller/purchase.controller';
 
 const router = Router();
 
-router.get('/cart', isAuth, purchaseController.index);
+router.get('/', isAuth, purchaseController.index);
 
 router.post('/cart', isAuth, purchaseController.addToCart);
 router.post('/checkout', isAuth, purchaseController.completePurchase);
